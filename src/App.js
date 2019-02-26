fc2e77db2f139499ea3af79dfe2f0e62f0dd7589
Pramathi <47630714+zealthy-node@users.noreply.github.com>
2019-02-26 18:35:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
+
+class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+     this.setState((prevState) => {
+       return {sideDrawerOpen: !prevState.sideDrawerOpen};
+     });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({sideDrawerOpen: false});
+  }
+  render() {
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler}/>
+    }
+
+    return (
+      <div style={{height: '100%'}}>
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
+        <SideDrawer show={this.state.sideDrawerOpen}/>
+        {backdrop}        
+        <main style={{marginTop: '64px'}}>
+        
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
