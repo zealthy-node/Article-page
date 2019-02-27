c3067e30cffef6c388fb6604fd7ee3571dcb1ad0
Pramathi <47630714+zealthy-node@users.noreply.github.com>
2019-02-27 14:14:37 +0530

    Update SideDrawer.js

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -7,13 +7,47 @@ const sideDrawer = props => {
         drawerClasses = 'side-drawer open';
     }
 
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
     return (
-   <nav className={drawerClasses}>
-       <ul>
-           <li><a href="/">Products</a></li>
-           <li><a href="/">Sign In</a></li>
-       </ul>
-   </nav>
+      <nav className={drawerClasses}>
+      <h1>CATEGORIES</h1>
+      <div className="category">
+        <ul>
+          <li>
+              <button class="dropdown-btn">Skin 
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-container">
+              <ul>
+               <li> <a href="#">Acne</a></li>
+                <li><a href="#">Oily Skin</a></li>
+                <li><a href="#">Dry Skin</a></li>
+              </ul>
+              </div>
+          </li>
+           <li><a href="#hairsub">Hair</a></li>
+           <li><a href="/">Menstrual Health and Menopause</a></li>
+           <li><a href="/">Sexual Wellness</a></li>
+           <li><a href="/">Infertility, Contraception and Birth Control</a></li>
+           <li><a href="/">Pregnency and Parenting</a></li>
+           <li><a href="/">Kids Health</a></li>
+        </ul>
+        </div>
+      </nav>
     );
 };
 export default sideDrawer;
